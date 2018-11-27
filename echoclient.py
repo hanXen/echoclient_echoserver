f4e203a4599cae8c26205effe02cdfdb190c6eb3
hanXen <shl9798@naver.com>
2018-11-27 09:55:46 -0800

    second commits

--- echoclient.py
+++ echoclient.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+
 import socket
 import sys
 import os
 import threading
 import time
 
-
-def receive(client_socket):
+def check_receive(client_socket):
 	while True:
 		try:
 			data = client_socket.recv(2048)
@@ -13,41 +14,44 @@ def receive(client_socket):
 				break
 			elif data.decode() == "terminate":
 				break
-			print("***received from server***")
+			print("*** received from server ***")
 			print(data.decode())
 			print("Input Request >> ")
 		except:
 			pass
 
-	print("---Server closed. Sorry :(---")
+	print("--- Server closed. Sorry :( ---")
 	client_socket.close()
 	os._exit(1)
 
+def start_client(client_socket ,addr):
+	print("+++ Hello, Client +++")
+	print('+++ Press Ctrl-C or Enter "exit plz" to exit program +++')
+	print("***",addr, "connected ***")
 
-def client(client_socket):
 	try:
-		t = threading.Thread(target = receive, args = (client_socket,))
+		t = threading.Thread(target = check_receive, args = (client_socket,))
 		t.daemon = True
 		t.start()
 		print("Input Request >> ")
 		while True:
 			data = input()
 			client_socket.send(data.encode())
-			if(data == "/exit"):
+			if(data == "exit plz"):
 				break
 			time.sleep(0.1)		
-	except :
+
+	except KeyboardInterrupt:
 		pass
 
 	finally:
-		print("***disconnected***")
+		print("\n***", addr, "disconnected ***")
 		client_socket.close()
 		sys.exit(0)
 
-
 def main():
 	if len(sys.argv) != 3:
-		print("***Syntax Error***")
+		print("*** Syntax Error ***")
 		print("Syntax: echoclient <host> <port>")
 		print("Sample: echoclient 127.0.0.1 6969")
 		sys.exit(1)
@@ -58,12 +62,11 @@ def main():
 		client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 		client_socket.connect(addr)
 	except Exception as e:
-		print("***Failed to connect***")
+		print("*** Failed to connect ***")
 		print(e)
 		sys.exit(1)
 
-	client(client_socket)
-
+	start_client(client_socket , addr)
 
 if __name__=='__main__':
 	main()
--- echoserver.py
+++ echoserver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import socket
 import sys
 import threading
@@ -21,13 +23,34 @@ def lis(client_socket,addr,opt_b):
 	clients_list.remove(client_socket)
 	client_socket.close()
 
+def start_server(server_socket,opt_b):
+	print("+++ Hello, Server +++")
+	print("+++ Press Ctrl-C to close server +++")
+	try:
+		while True:
+			print("listening for connection")
+			(client_socket,addr) = server_socket.accept()
+			clients_list.append(client_socket)
+			print("***",addr,"connected ***")
+			t = threading.Thread(target = lis, args = (client_socket,addr,opt_b))
+			t.start()
+
+	except KeyboardInterrupt:
+		print("\n--- Server Closed ---")
+		for client in clients_list:
+			client.send("terminate".encode())
+			client.close()
+
+		server_socket.close()
+		sys.exit(1)
+
 
 def main():
 	opt_b = False
 	if len(sys.argv) == 3 and sys.argv[2] == '-b':
 		opt_b = True
 	elif len(sys.argv) != 2:
-		print("***Syntax Error***")
+		print("*** Syntax Error ***")
 		print("Syntax: echoserver <port>")
 		print("Sample: echoserver 8080")
 		sys.exit(1)
@@ -39,24 +62,7 @@ def main():
 	server_socket.bind(('127.0.0.1',port))
 	server_socket.listen(5)
 
-	try:
-		while True:
-			print("listening for connection")
-			(client_socket,addr) = server_socket.accept()
-			clients_list.append(client_socket)
-			print("***",addr,"connected***")
-			t = threading.Thread(target = lis, args = (client_socket,addr,opt_b))
-			t.start()
-
-	except KeyboardInterrupt:
-		print("Server Closed")
-		for client in clients_list:
-			client.send("terminate".encode())
-			client.close()
-
-		server_socket.close()
-		sys.exit(1)
-		
+	start_server(server_socket,opt_b)
 
 	server_socket.close()
 
